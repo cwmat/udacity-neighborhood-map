f6345d763d02bef03154d26088bcf33c527faee8
cwmateer <cwmateer@ncsu.edu>
2015-09-29 21:14:15 -0400

    test: Figured out how to get a value returned form a callback function.

--- js/map.js
+++ js/map.js
@@ -8,40 +8,27 @@ function initMap() {
     zoom: 13,
   });
 
-  var initialPlaces = [
-    {
+  var initialPlaces = [{
     name: "Sammy T's",
     placeID: '',
     imgSrc: '',
     blurb: '',
-    },
-    {
+  }, {
     name: "J Brian's",
     placeID: '',
     imgSrc: '',
     blurb: '',
-    },
-    {
+  }, {
     name: "Paul's Bakery",
     placeID: '',
     imgSrc: '',
     blurb: '',
-    },
-  ]
+  }, ]
 
-  // Checks that the PlacesServiceStatus is OK, and adds a marker
-  // using the place ID and location from the PlacesService.
-  function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      // console.log(results[0].place_id);
-      return results[0].place_id;
-      // obj.placeID = results[0].place_id;
 
-    }
-  }
 
   // Used to request the palce id based on common name of a place
-  var getPlaceID = function(searchQuery) {
+  var getPlaceID = function(ogPlace, searchQuery, fn) {
     var request = {
       location: {
         lat: 38.300829,
@@ -51,14 +38,65 @@ function initMap() {
       query: searchQuery
     };
 
+    // console.log(ogPlace);
     var service = new google.maps.places.PlacesService(map);
-    // var placeID = service.textSearch(request, callback);
-    console.log(service.textSearch(request, callback));
-    // console.log(placeID);
-    // obj.placeID = placeID;
-    // return placeID;
+    service.textSearch(request, callback);
+
+    // Checks that the PlacesServiceStatus is OK, and adds a marker
+    // using the place ID and location from the PlacesService.
+    function callback(results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        var marker = new google.maps.Marker({
+          map: map,
+          place: {
+            placeId: results[0].place_id,
+            location: results[0].geometry.location
+          }
+        });
+        fn(results[0].place_id);
+        // Make and attach infowindow
+        var request = {
+          placeId: results[0].place_id
+        };
+        var service = new google.maps.places.PlacesService(map);
+        var infowindow = new google.maps.InfoWindow();
+        service.getDetails(request, function(place, status) {
+          if (status == google.maps.places.PlacesServiceStatus.OK) {
+            // If the request succeeds, draw the place location on the map
+            // as a marker, and register an event to handle a click on the marker.
+            marker.addListener('click', function() {
+              infowindow.setContent(place.name);
+              infowindow.open(map, marker);
+            });
+
+
+            // google.maps.event.addListener(marker, 'click', function() {
+            //   infowindow.setContent(place.name);
+            //   infowindow.open(map, this);
+            // });
+
+          }
+
+        });
+
+
+      }
+      // console.log(ogPlace);
+      ogPlace.placeID = results[0].place_id;
+      // console.log(ogPlace);
+      // console.log(ogPlace.placeID);
+    }
   }
 
+  initialPlaces.forEach(function(place) {
+    // console.log(place);
+    getPlaceID(place, place.name, function(placeThing) {
+      // OMG OMG OMG NO WAY!
+      // http://stackoverflow.com/questions/6847697/how-to-return-value-from-an-asynchronous-callback-function
+      console.log(placeThing);
+    });
+    // console.log(place);
+  });
 
 
   // // Load data into initialPLaces
@@ -72,7 +110,7 @@ function initMap() {
 
 
 
- ///////////////////
+  ///////////////////
 
   // initialPlaces.forEach(function(place) {
   //   var request = {
@@ -102,39 +140,34 @@ function initMap() {
 
 
 
-  for (var i = 0; i < initialPlaces.length; i++) {
-    var request = {
-      location: {
-        lat: 38.300829,
-        lng: -77.486540
-      },
-      radius: '500',
-      query: initialPlaces[i].name
-    };
-
-    var service = new google.maps.places.PlacesService(map);
-    // var placeID = service.textSearch(request, callback);
-    service.textSearch(request, callback);
-
-    function callback(results, status) {
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
-        // console.log(results[0].place_id);
-        //return results[0].place_id;
-        results[0].place_id;
-        // obj.placeID = results[0].place_id;
-
-      }
-    }.done(function(results) {
-      console.log(results);
-    });
-  }
-
-  initialPlaces.forEach(function(place) {
-    console.log(place.placeID);
-  });
-
-
-
+  // for (var i = 0; i < initialPlaces.length; i++) {
+  //   var request = {
+  //     location: {
+  //       lat: 38.300829,
+  //       lng: -77.486540
+  //     },
+  //     radius: '500',
+  //     query: initialPlaces[i].name
+  //   };
+  //
+  //   var service = new google.maps.places.PlacesService(map);
+  //   // var placeID = service.textSearch(request, callback);
+  //   service.textSearch(request, callback);
+  //
+  //   function callback(results, status) {
+  //     if (status == google.maps.places.PlacesServiceStatus.OK) {
+  //       // console.log(results[0].place_id);
+  //       //return results[0].place_id;
+  //       results[0].place_id;
+  //       // obj.placeID = results[0].place_id;
+  //
+  //     }
+  //   }
+  // }
+  //
+  // initialPlaces.forEach(function(place) {
+  //   console.log(place.placeID);
+  // });
 
 
 
